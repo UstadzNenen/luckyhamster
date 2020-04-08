7c56af4bcd61b2375372b3cffb6ca768f03376bb
Denny Kresno Sunarno <63137098+UstadzNenen@users.noreply.github.com>
2020-04-09 06:05:54 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+
+//Please-Remember-If-The-Script-Not-Work-Recapture-Data-Again
+
+
+$Authorization = 'MjM3MzkyNTYtMjI2Ny00MGZiLTg1MTItNmI5ZmM2MTJkZTA3';
+
+
+
+$sha1_hash = 'com.makemoney.knock.ice&&&q7yVPfih0op6D3QuYlt82fxln6M=';
